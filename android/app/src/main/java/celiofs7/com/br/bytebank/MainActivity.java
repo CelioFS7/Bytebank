a0ea7ca36b898b990e6f86757da38cb7e9842298
Celio <celioferreiramlm@gmail.com>
2021-10-26 16:47:20 -0300

    Fist page

---
+++ android/app/src/main/java/celiofs7/com/br/bytebank/MainActivity.java
@@ -0,0 +1,6 @@
+package celiofs7.com.br.bytebank;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          body: ListaTransferencias(),
+
+          appBar: AppBar(
+            title: const Text('Transferências'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
+
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+
+        ItemTransferencia(valor: '100',numeroConta:'548963'),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final String valor;
+  final String numeroConta;
+
+  const ItemTransferencia({Key? key, required this.valor, required this.numeroConta}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return  Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(valor),
+        subtitle: Text(numeroConta),
+      ),
+    );
+  }
+}
+
